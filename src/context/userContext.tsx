aa28656ad096effecbc7fd1a0e8d5540034d22b8
lchavez <lucio.chavez031093@gmail.com>
2026-05-20 02:31:01 -0600

    Lucio - estructurando contexto

--- src/context/userContext.tsx
+++ src/context/userContext.tsx
@@ -1,7 +1,39 @@
-import type { PropsWithChildren } from "react"
+import type { User } from "@/actions/PostDatosLogin"
+import { createContext, useState, type PropsWithChildren } from "react"
+
+interface UserContextProps {
+    authStatus: "checando" | "autenticado" | "no-autenticado",
+    user: User | null,
+    token: string | null,
+
+    login: (email: string, password: string) => boolean,
+    logout: () => void,
+}
+
+export const userContext = createContext({} as UserContextProps);
 
 export const UserContextProvider = ({ children }: PropsWithChildren) => {
-    return <>
+
+    const [authStatus, setAuthStatus] = useState<"checando" | "autenticado" | "no-autenticado">("checando");
+    const [user, setUser] = useState<User | null>(null);
+    const [token, setToken] = useState<string | null>(null);
+
+    const handleLogin = (email: string, password: string) => {
+        return true;
+    }
+
+    const handleLogout = () => {
+        return true;
+    }
+
+    return <userContext.Provider value={{
+        authStatus,
+        user,
+        token,
+
+        login: handleLogin,
+        logout: handleLogout
+    }}>
         {children}
-    </>
+    </userContext.Provider>
 }
